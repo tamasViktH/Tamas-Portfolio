0a9eb26223682ef76b5f6230933c47d0793b5eaa
tamasViktH <tamasviktorhazos@gmail.com>
2025-06-04 17:26:12 +0200

    changes in responsiveness

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+    document.body.style.overflow = document.body.classList.contains('nav-open') ? 'hidden' : 'auto';
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
-    })
-})
+        document.body.style.overflow = 'hidden';
+    });
+});
